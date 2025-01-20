cebbd47f5f0516d0d2ba3177dbe3b4b7058a6def
LordAnthrax94 <andreagi1994@gmail.com>
2025-01-20 18:39:10 +0100

    fix: aggiustati inserimenti del form e le card

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import libri from "./array";
+import libri from "./libri";
 
-const taskList = libri;
+const listaLibri = libri;
 
 const Main = ()=>{  
 
-const [newTask, setNewTask] = useState(taskList);
+const [taskList, setTaskList] = useState(listaLibri);
 const [formData, setformData] = useState({   
     "id": "",
     "titolo": "",
@@ -18,39 +18,40 @@ const [formData, setformData] = useState({
 
  const handlerSubmit = (e) => {
     e.preventDefault();
-    setNewTask([formData, ...newTask]);
+    setTaskList([{...formData, id: Date.now()}, ...taskList]);
   };
 
   const updateTask = (e) =>{
-    const formData = {
+    const articleData = {
       ...formData,
       [e.target.name]: e.target.value
     };
-    setformData(formData);
+
+    setformData(articleData);
   }
   
   const handlerRemove = (id) =>{
-    const removeList = newTask.filter((item) => item.id !== id)
-    setNewTask(removeList);
+    const removeList = taskList.filter((item) => item.id !== id)
+    setTaskList(removeList);
   };
 
   return (
    <main>
     <div className="container">
-      <form action="#" onSubmit={ handlerSubmit }>
+      <form action="#" onSubmit={handlerSubmit}>
         <div className="col-12">
           <div className="input-group mb-3 mt-5">
-            <input name="text" type="text" className="form-control" placeholder="Aggiungi nuovo articolo" value={formData.text} onChange={updateTask} />          
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Aggiungi</button>
+            <input name="titolo" type="text" className="form-control" placeholder="Aggiungi nuovo articolo" value={formData.text} onChange={updateTask} />          
+            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Aggiungi</button>
           </div>
           
             <div className="mb-3">
-              <label for="formFileDisabled" className="form-label">Aggiungi immagine</label>
+              <label hmtlfor="formFileDisabled" className="form-label">Aggiungi immagine</label>
               <input name="immagine" className="form-control" type="file" id="formFileDisabled" disabled />
             </div>          
 
           <div className="mb-3">
-            <label for="exampleFormControlTextarea1" className="form-label">Inserire contenuto</label>
+            <label hmtlfor="exampleFormControlTextarea1" className="form-label">Inserire contenuto</label>
             <textarea name="contenuto" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
           </div>
         </div>       
@@ -58,19 +59,15 @@ const [formData, setformData] = useState({
        
       <div className="container">
         <div className="col-12">
-          {newTask.map((task) => (<li key={task.id}>{task.text} <i className="fa-solid fa-trash" onClick={() => handlerRemove(task.id)}></i></li>))}
-            <div className="card">
-              <img src="..." class="card-img-top" alt="..." />
+          {taskList.map((task) => (<div className="card mt-5" key={task.id}>{task.titolo} <i className="fa-solid fa-trash" onClick={() => handlerRemove(task.id)}></i>
+           
+              <img src={task.immagini} className="card-img-top" alt="immagine" />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-              </ul>            
-            </div>
+                <h5 className="card-title">{task.titolo}</h5>
+                <p className="card-text">{task.contenuto}</p>
+              </div>                        
+            
+            </div>))}
 
         </div>        
       </div>     

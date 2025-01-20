2d13fa1bbd3e9102b63b127a11e305f89b80bf4c
LordAnthrax94 <andreagi1994@gmail.com>
2025-01-20 17:29:45 +0100

    fix: inseriti form per inserimento dati dell'oggetto e aggiunta card

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
+import libri from "./array";
 
-const taskList = [];
+const taskList = libri;
 
 const Main = ()=>{  
 
 const [newTask, setNewTask] = useState(taskList);
-const [addTask, setAddTask] = useState({ text: "" })
+const [formData, setformData] = useState({   
+    "id": "",
+    "titolo": "",
+    "contenuto": "",
+    "categoria": "",
+    "immagini": "",
+    "tags": [],
+    "stato": ""  
+})
 
  const handlerSubmit = (e) => {
     e.preventDefault();
-    setNewTask([addTask, ...newTask]);
+    setNewTask([formData, ...newTask]);
   };
 
   const updateTask = (e) =>{
-    const addTask = {
-      id: Date.now(),
-      text: e.target.value
+    const formData = {
+      ...formData,
+      [e.target.name]: e.target.value
     };
-    setAddTask(addTask);
+    setformData(formData);
   }
   
   const handlerRemove = (id) =>{
@@ -29,16 +38,41 @@ const [addTask, setAddTask] = useState({ text: "" })
    <main>
     <div className="container">
       <form action="#" onSubmit={ handlerSubmit }>
-        <div className="input-group mb-3 mt-5">
-          <input type="text" className="form-control" placeholder="Inserire la nuova Task" value={addTask.text} onChange={updateTask} />
-          <button className="btn btn-outline-secondary" type="button" id="button-addon2">Aggiungi</button>
-        </div>
-      </form> 
+        <div className="col-12">
+          <div className="input-group mb-3 mt-5">
+            <input name="text" type="text" className="form-control" placeholder="Aggiungi nuovo articolo" value={formData.text} onChange={updateTask} />          
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Aggiungi</button>
+          </div>
+          
+            <div className="mb-3">
+              <label for="formFileDisabled" className="form-label">Aggiungi immagine</label>
+              <input name="immagine" className="form-control" type="file" id="formFileDisabled" disabled />
+            </div>          
+
+          <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">Inserire contenuto</label>
+            <textarea name="contenuto" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        </div>       
+      </form>
+       
       <div className="container">
-        <ul className="list-group">
+        <div className="col-12">
           {newTask.map((task) => (<li key={task.id}>{task.text} <i className="fa-solid fa-trash" onClick={() => handlerRemove(task.id)}></i></li>))}
-                  
-        </ul>
+            <div className="card">
+              <img src="..." class="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+              </ul>            
+            </div>
+
+        </div>        
       </div>     
     </div>
       
---
+++ src/Components/array.js
@@ -0,0 +1,49 @@
+const libri =[
+  {
+    "id": 1,
+    "titolo": "Sfumature di Blu",
+    "contenuto": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eligendi ipsum. Culpa consequatur, placeat beatae atque unde modi ad ullam.",
+    "categoria": "Arte",
+    "immagini": "image1.jpg",
+    "tags": ["astratto", "blu", "modernismo"],
+    "stato": "pubblicato"
+  },
+  {
+    "id": 2,
+    "titolo": "Racconto Invernale",
+    "contenuto": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eligendi ipsum. Culpa consequatur, placeat beatae atque unde modi ad ullam.",
+    "categoria": "Letteratura",
+    "immagini": "book_cover1.jpg",
+    "tags": ["inverno", "narrativa", "storia"],
+    "stato": "bozza"
+  },
+  {
+    "id": 3,
+    "titolo": "Innovazioni tecnologiche",
+    "contenuto": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eligendi ipsum. Culpa consequatur, placeat beatae atque unde modi ad ullam.",
+    "categoria": "Tecnologia",
+    "immagini": "tech_image1.jpg",
+    "tags": ["AI", "futuro", "innovazione"],
+    "stato": "pubblicato"
+  },
+  {
+    "id": 4,
+    "titolo": "Giro del Mondo",
+    "contenuto": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eligendi ipsum. Culpa consequatur, placeat beatae atque unde modi ad ullam.",
+    "categoria": "Viaggi",
+    "immagini": "travel1.jpg",
+    "tags": ["avventura", "destinazioni", "viaggi"],
+    "stato": "bozza"
+  },
+  {
+    "id": 5,
+    "titolo": "Fitness e Benessere",
+    "contenuto": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eligendi ipsum. Culpa consequatur, placeat beatae atque unde modi ad ullam.",
+    "categoria": "Salute",
+    "immagini": "fitness1.jpg",
+    "tags": ["salute", "esercizio", "benessere"],
+    "stato": "pubblicato"
+  }
+]
+
+export default libri;
